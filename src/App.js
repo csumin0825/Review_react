4cc2733a930c7be225baf85b58eacb5c2fba1582
Sumin <csumin008255@gmail.com>
2021-09-08 18:13:48 +0900

    Change App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <b>최수민의 복습 프로젝트</b>
     </div>
   );
 }
